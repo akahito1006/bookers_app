292a47ef42e5716c361b9f3d4acc791e62fa881d
nekodora5k@gmail.com <ec2-user@ip-172-31-8-33.ec2.internal>
2021-02-03 09:19:49 +0000

    [Update] 3-8 index, new, routes error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :blogs
+  get 'index' => 'books#index'
+  get 'books' => 'books#books'
+  get 'books' => 'books#new'
+  post 'books' => 'books#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
