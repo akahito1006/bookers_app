b219a374e244cbddb0e699113e005d5928bb4c73
akahito1006 <nekodora5k@gmail.com>
2021-02-03 11:33:16 +0000

    [Update] 3-8 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'index' => 'books#index'
-  get 'books' => 'books#books'
-  get 'books' => 'books#new'
-  post 'books' => 'books#create'
+  get 'books/index' => 'books#index'
+  get 'books/books' => 'books#books'
+  get 'books/books' => 'books#new'
+  post 'books/books' => 'books#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
